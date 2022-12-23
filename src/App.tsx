49f4675dfa5c4b8c22d96ac6a9eca01e90975440
Eduardo-H <edu-henrique2014@hotmail.com>
2022-12-23 12:46:42 -0300

    Sidebar component implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { Header } from './components/Header';
 
+import styles from './app.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          post
+        </main>
+      </div>
     </>
   );
 }
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,31 @@
+import { PencilSimpleLine } from 'phosphor-react';
+
+import styles from './styles.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.container}>
+      <img 
+        src="https://images.unsplash.com/photo-1617900906639-cab7adceb499?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+        className={styles.cover}
+      />
+
+      <div className={styles.profile}>
+        <img 
+          src="https://github.com/Eduardo-H.png" 
+          className={styles.avatar}  
+        />
+
+        <strong>Eduardo Oliveira</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilSimpleLine size={20} weight="regular" />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
